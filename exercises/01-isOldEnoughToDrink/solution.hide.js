95ad93d94b3e628bdc006fea72730389cb74b825
Eliud <eliudjgomezg@gmail.com>
2020-12-05 09:09:08 -0300

    delete solutions

--- exercises/01-isOldEnoughToDrink/solution.hide.js
+++
@@ -1,5 +0,0 @@
-function isOldEnoughToDrink(age) {
-  // your code here	
-  if(age > 20) return true;
-  else return false;
-}
--- exercises/03-isOldEnoughToVote/solution.hide.js
+++
@@ -1,5 +0,0 @@
-function isOldEnoughToVote(age) {
-  // your code here
-  if(age >20) return true;
-  else return false;
-}
